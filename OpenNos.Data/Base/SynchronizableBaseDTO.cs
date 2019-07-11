7ed6d7b372da827e14e2519ff9d0e1efe24e2159
Lvxweb <50013828+Lvxweb@users.noreply.github.com>
2019-07-11 13:34:32 +0200

    Add files via upload

---
+++ OpenNos.Data/Base/SynchronizableBaseDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenNos.Data
+{
+    public abstract class SynchronizableBaseDTO : MappingBaseDTO
+    {
+        #region Instantiation
+
+        public SynchronizableBaseDTO()
+        {
+            Id = Guid.NewGuid(); //make unique
+        }
+
+        #endregion
+
+        #region Properties
+
+        [Key]
+        public Guid Id { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override bool Equals(object obj)
+        {
+            return ((SynchronizableBaseDTO)obj).Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        #endregion
+    }
+}
